40516ef816270470432e2ce8a6bfbfeac7bd890d
James Eddy <james.a.eddy@gmail.com>
2019-04-02 21:27:25 -0700

    add OpenAPI generated server python library

---
+++ openapi_server/models/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from openapi_server.models.json_activity import JsonActivity
+from openapi_server.models.json_paginated_results_of_reference import JsonPaginatedResultsOfReference
+from openapi_server.models.json_reference import JsonReference
+from openapi_server.models.json_used import JsonUsed
