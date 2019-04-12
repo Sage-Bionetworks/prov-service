65a056a0b84ffa3775947e7edc8c660ad81ba332
James Eddy <james.a.eddy@gmail.com>
2019-04-11 18:51:57 -0700

    update README and requirements

--- synprov/config.py
+++ synprov/config.py
@@ -2,7 +2,6 @@
 
 import connexion
 
-from flask_pymongo import PyMongo
 from flask_mongoengine import MongoEngine
 from synprov import encoder
 
@@ -10,10 +9,9 @@ from synprov import encoder
 connex_app = connexion.App(__name__, specification_dir='./openapi/')
 
 app = connex_app.app
-# app.config['MONGO_URI'] = 'mongodb://localhost:27017/prov_db'
 app.config['MONGODB_SETTINGS'] = {
-    'db': 'prov_db',
-    'host': 'mongodb://localhost:27017/prov_db',
+    'db': 'provDB',
+    'host': 'mongodb://localhost:27017/provDB',
     'port': 27017
 }
 app.json_encoder = encoder.JSONEncoder
