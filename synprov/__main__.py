981e8e09ff8c7311ffad7480c313d6072fa93f9d
James Eddy <james.a.eddy@gmail.com>
2019-04-11 17:22:31 -0700

    Wire up GraphQL endpoint
    Added schema and root query for db using graphene_mongoengine library
    for Flask. Also added a simple db pre-population module.

--- synprov/__main__.py
+++ synprov/__main__.py
@@ -3,23 +3,65 @@
 import connexion
 
 from flask_graphql import GraphQLView
-from graphene import Schema
 
 from synprov import config
-# from synprov.schema import Query
+from synprov.database import init_db
+from synprov.schema import schema
 
 
 def main():
     app = config.connex_app
     app.add_api('openapi.yaml', arguments={'title': 'Provenance Service'})
-    # app.app.add_url_rule(
-    #     '/graphql',
-    #     view_func=GraphQLView.as_view('graphql', 
-    #                                   schema=Schema(query=Query), 
-    #                                   graphiql=True)
-    # )
-    app.run(port=8080)
 
+    default_query = '''
+    {
+      allActivities {
+        edges {
+          node {
+            id,
+            activityId,
+            name,
+            description,
+            used {
+              edges {
+                node {
+                  id,
+                  targetId,
+                  targetVersionNumber
+                }
+              }
+            },
+            generated {
+              edges {
+                node {
+                  id,
+                  targetId,
+                  targetVersionNumber
+                }
+              }
+            },
+            agents {
+              edges {
+                node{
+                  id,
+                  agentId
+                }
+              }
+            }
+          }
+        }
+      }
+    }'''.strip()
+
+    app.app.add_url_rule(
+        '/graphql',
+        view_func=GraphQLView.as_view('graphql', 
+                                      schema=schema, 
+                                      graphiql=True)
+    )
+
+    init_db()
+    app.run(port=8080)
 
 if __name__ == '__main__':
     main()
