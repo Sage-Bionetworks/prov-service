b111d4b4f8c7fc69713fc80fdeee007022e6401e
James Eddy <james.a.eddy@gmail.com>
2019-04-11 19:58:00 -0700

    add docker-compose script

--- synprov/config.py
+++ synprov/config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import connexion
 
 from flask_mongoengine import MongoEngine
@@ -8,9 +9,12 @@ from flask_mongoengine import MongoEngine
 connex_app = connexion.App(__name__, specification_dir='./openapi/')
 
 app = connex_app.app
+local_uri = 'mongodb://localhost:27017/provDB'
+env_uri = os.environ.get('MONGODB_URI')
+mongodb_host = env_uri if env_uri is not None else local_uri
 app.config['MONGODB_SETTINGS'] = {
     'db': 'provDB',
-    'host': 'mongodb://localhost:27017/provDB',
+    'host': mongodb_host,
     'port': 27017
 }
 
