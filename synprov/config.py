88bd1b8421a1ae709076069f80afe59c6ca38b96
James Eddy <james.a.eddy@gmail.com>
2019-06-13 13:31:54 -0700

    use env vars for neo4j username/password

--- synprov/config.py
+++ synprov/config.py
@@ -11,9 +11,12 @@ connex_app = connexion.App(__name__, specification_dir='./openapi/')
 env_host = os.environ.get('NEO4J_HOST')
 neo4j_host = env_host if env_host is not None else 'localhost'
 
+neo_user = os.environ['NEO4J_USERNAME']
+neo_pass = os.environ['NEO4J_PASSWORD']
+
 neomod = NeoModel(connex_app.app, variables={
-    'user': 'neo4j',
-    'password': 'mypassword',
+    'user': neo_user,
+    'password': neo_pass,
     'host': neo4j_host,
     'port': 7687
 })
