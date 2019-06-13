029a16f8de3df547aa680902e4b9b50748ff591c
James Eddy <james.a.eddy@gmail.com>
2019-06-13 16:08:07 -0700

    silence old nosetests, switch to pytest

--- synprov/test/__init__.py
+++ synprov/test/__init__.py
@@ -1,13 +1,13 @@
 import logging
 
-import connexion
-from flask_testing import TestCase
+# import connexion
+# from flask_testing import TestCase
 
 
-class BaseTestCase(TestCase):
+# class BaseTestCase(TestCase):
 
-    def create_app(self):
-        logging.getLogger('connexion.operation').setLevel('ERROR')
-        app = connexion.App(__name__, specification_dir='../openapi/')
-        app.add_api('openapi.yaml')
-        return app.app
+#     def create_app(self):
+#         logging.getLogger('connexion.operation').setLevel('ERROR')
+#         app = connexion.App(__name__, specification_dir='../openapi/')
+#         app.add_api('openapi.yaml')
+#         return app.app
