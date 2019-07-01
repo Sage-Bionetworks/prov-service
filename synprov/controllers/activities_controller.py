0d3d7403fe8bb1f02a73444ca84aae444eec387c
James Eddy <james.a.eddy@gmail.com>
2019-07-01 15:47:30 -0700

    add operation to return graph in d3-compatible json format

--- synprov/controllers/activities_controller.py
+++ synprov/controllers/activities_controller.py
@@ -2,11 +2,13 @@ import connexion
 import six
 import uuid
 import humps
+import json
 
 from py2neo import Graph, Node, NodeMatcher
 
 from synprov.config import neomod
 from synprov.graphmodels import Activity, Reference, Agent
+from synprov.util import neo4j_to_d3
 
 
 graph = Graph(neomod.neo.db.url)
@@ -22,19 +24,31 @@ def create_activity(body):  # noqa: E501
 
     :rtype: Activity
     """
-    act = Activity(**humps.decamelize(body))
-    act.save()
+    # act = Activity(**humps.decamelize(body))
+    # act.save()
 
-    return act.node
-
-
-def get_activities_graph():  # noqa: E501
-    """Get provenance graph.
-
-    Retrieve all nodes and relationships in the graph that pass filters. # noqa: E501
-
-
-    :rtype: Graph
-    """
+    # return act.node
     return 'Not Implemented', 501
 
+
+def get_activities_graph(limit=20):  # noqa: E501
+    """Get provenance graph
+
+    Retrieve all nodes and relationships in the graph that pass filters.  # noqa: E501
+
+    :param limit: maximum number of nodes to return
+    :type limit: int
+
+    :rtype: D3Graph
+    """
+    # TODO: with the current query, the 'limit' parameter applies
+    # to relationships, not really to nodes
+    results = graph.run(
+        '''
+        MATCH (s)-[r]-(t)
+        RETURN s as source, r as relationship, t as target
+        LIMIT {limit}
+        ''',
+        limit=limit
+    )
+    return neo4j_to_d3(results.data())
--- synprov/util.py
+++ synprov/util.py
@@ -10,7 +10,7 @@ def _convert_keys(obj):
     """
     Convert keys in a dictionary (or nested dictionary) from snake_case
     to camelCase; ignore '_id' keys.
-    
+
     :param obj: A dict or list of dicts with string keys to be
         converted.
     :type obj: dict, list
@@ -32,3 +32,33 @@ def _convert_keys(obj):
 def create_hash(values):
     return sha256(bytes(''.join(values), 'utf8')).hexdigest()
 
+
+def neo4j_to_d3(results):
+    nodes = []
+    rels = []
+    for record in results:
+        nodes.append(_convert_node(record['source']))
+        nodes.append(_convert_node(record['target']))
+        rels.append(_convert_relationship(record['relationship']))
+    return json.dumps({"nodes": nodes, "links": rels})
+
+
+def _convert_node(neo4j_node):
+    return {
+        'id': neo4j_node.identity,
+        'labels': list(neo4j_node.labels),
+        'properties': neo4j_node
+    }
+
+
+def _convert_relationship(neo4j_rel):
+    return {
+        'id': neo4j_rel.identity,
+        'type': list(neo4j_rel.types()),
+        'startNode': neo4j_rel.start_node.identity,
+        'endNode': neo4j_rel.end_node.identity,
+        'properties': neo4j_rel,
+        'source': neo4j_rel.start_node.identity,
+        'target': neo4j_rel.end_node.identity,
+        'linknum': 1
+    }
