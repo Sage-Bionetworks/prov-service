65fc97c2d09c8a8089f876372eceb715b3034afa
James Eddy <james.a.eddy@gmail.com>
2019-07-12 20:48:59 -0700

    move controller implementations to graph package

---
+++ synprov/graph/controllers/references_controller.py
@@ -0,0 +1,25 @@
+import connexion
+import six
+import uuid
+import humps
+
+from py2neo import Graph, Node, NodeMatcher
+
+from synprov.config import neomod
+from synprov.graph import GraphActivity, GraphReference, GraphAgent
+
+
+graph = Graph(neomod.neo.db.url)
+
+
+def get_reference_subgraph(id):  # noqa: E501
+    """Get subgraph connected to an entity.
+
+    Retrieve the nodes and relationships in a neighborhood around a specified entity. # noqa: E501
+
+    :param id: The ID of the entity to fetch.
+    :type id: str
+
+    :rtype: Graph
+    """
+    return 'Not Implemented', 501
