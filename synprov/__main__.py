0ed2ea1d2bff86d2b04ae6569ad239551bcd07cd
James Eddy <james.a.eddy@gmail.com>
2019-07-12 17:24:24 -0700

    update app init to use latest mocking logic

--- synprov/__main__.py
+++ synprov/__main__.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 
 import connexion
+from py2neo import Graph
 
 from synprov import create_app
-from synprov.graphdb import init_db
+from synprov.config import neomod
+from synprov.graph.client import GraphClient
+from synprov.mock.main import create_mock_graph
+
+
+graph = Graph(neomod.neo.db.url)
+
+
+def init_db(num_activities=30):
+    graph.run(
+        '''
+        MATCH (n)
+        DETACH DELETE n
+        '''
+    )
+    create_mock_graph(GraphClient(graph), num_activities)
 
 
 def main():
@@ -11,6 +27,7 @@ def main():
 
     init_db()
     app.run(host='localhost', port=8080, debug=True)
+
 
 if __name__ == '__main__':
     main()
