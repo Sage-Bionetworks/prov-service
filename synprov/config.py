76859b2d16992e7d5d5cc9194a8837d671fe1fde
James Eddy <james.a.eddy@gmail.com>
2019-07-15 17:35:56 -0700

    update app config to use runtime env vars for neo4j

--- synprov/config.py
+++ synprov/config.py
@@ -13,13 +13,15 @@ connex_app = connexion.App(__name__, specification_dir='./openapi/')
 
 env_host = os.environ.get('NEO4J_HOST')
 neo4j_host = env_host if env_host is not None else '0.0.0.0'
+env_scheme = os.environ.get('NEO4J_SCHEME')
+neo4j_scheme = env_scheme if env_scheme is not None else 'bolt'
 
 neo_user = os.environ['NEO4J_USERNAME']
 neo_pass = os.environ['NEO4J_PASSWORD']
 
 logger.info("Connecting to neo4j database")
 neo4j_connection = Graph(
-    scheme='bolt',
+    scheme=neo4j_scheme,
     host=neo4j_host,
     user=neo_user,
     password=neo_pass
