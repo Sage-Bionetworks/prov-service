fd8809e7ff3f8e8c1c48f9fa090f5f43e00d7f1c
James Eddy <james.a.eddy@gmail.com>
2019-07-18 22:49:50 -0700

    add basic controllers for ref/agt subgraph endpoints

--- synprov/graph/controllers/agents_controller.py
+++ synprov/graph/controllers/agents_controller.py
@@ -7,9 +7,10 @@ from py2neo import Node, NodeMatcher
 
 from synprov.config import neo4j_connection as graph
 from synprov.graph import GraphActivity, GraphReference, GraphAgent
+from synprov.util import neo4j_to_d3
 
 
-def get_agent_subgraph(id, limit=None):  # noqa: E501
+def get_agent_subgraph(id, limit=3, direction='down'):  # noqa: E501
     """Get subgraph connected to an agent.
 
     Retrieve all nodes and relationships in the graph that pass filters. # noqa: E501
@@ -19,4 +20,24 @@ def get_agent_subgraph(id, limit=None):  # noqa: E501
 
     :rtype: Graph
     """
-    return 'Not Implemented', 501
+    direction_rels = {
+        'down': '<-[r:WASASSOCIATEDWITH]-'
+    }
+    query_base = (
+        '''
+        MATCH (t:Agent {{user_id: {{id}}}}){dir_rel}(s:Activity)
+        WITH collect(s) as activities
+        UNWIND activities[0..{lim}] as t
+        MATCH (s)-[r]-(t)
+        RETURN s as source,r as relationship,t as target
+        '''
+    ).format(
+        lim=limit,
+        dir_rel=direction_rels[direction]
+    )
+
+    results = graph.run(
+        query_base,
+        id=id
+    )
+    return neo4j_to_d3(results.data())
--- synprov/graph/controllers/references_controller.py
+++ synprov/graph/controllers/references_controller.py
@@ -7,9 +7,10 @@ from py2neo import Node, NodeMatcher
 
 from synprov.config import neo4j_connection as graph
 from synprov.graph import GraphActivity, GraphReference, GraphAgent
+from synprov.util import neo4j_to_d3
 
 
-def get_reference_subgraph(id, limit=None, direction=None):  # noqa: E501
+def get_reference_subgraph(id, limit=3, direction='down'):  # noqa: E501
     """Get subgraph connected to an entity.
 
     Retrieve the nodes and relationships in a neighborhood around a specified entity. # noqa: E501
@@ -19,4 +20,25 @@ def get_reference_subgraph(id, limit=None, direction=None):  # noqa: E501
 
     :rtype: Graph
     """
-    return 'Not Implemented', 501
+    direction_rels = {
+        'up': '-[r:WASGENERATEDBY]->',
+        'down': '<-[r:USED]-'
+    }
+    query_base = (
+        '''
+        MATCH (t:Reference {{target_id: {{id}}}}){dir_rel}(s:Activity)
+        WITH collect(s) as activities
+        UNWIND activities[0..{lim}] as t
+        MATCH (s)-[r]-(t)
+        RETURN s as source,r as relationship,t as target
+        '''
+    ).format(
+        lim=limit,
+        dir_rel=direction_rels[direction]
+    )
+
+    results = graph.run(
+        query_base,
+        id=id
+    )
+    return neo4j_to_d3(results.data())
