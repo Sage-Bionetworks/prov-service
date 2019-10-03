d8a7c46d36791705254b3837ba4f478fe796702e
James Eddy <james.a.eddy@gmail.com>
2019-10-03 13:45:59 -0700

    update flask config to use pythonic params

--- synprov/__init__.py
+++ synprov/__init__.py
@@ -23,7 +23,9 @@ def neo4j_available():
 
 def create_app():
     app = connex_app
-    app.add_api('openapi.yaml', arguments={'title': 'Provenance Service'})
+    app.add_api('openapi.yaml',
+                arguments={'title': 'Provenance Service'},
+                pythonic_params=True)
     # wrap the flask app and give a heathcheck url
     health = HealthCheck(app, "/healthcheck")
     health.add_check(neo4j_available)
