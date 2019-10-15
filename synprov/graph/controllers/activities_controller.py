c3386f4087640bbccaeae1a61543551ae56ede55
James Eddy <james.a.eddy@gmail.com>
2019-10-15 16:45:28 -0700

    add filter logic to internal/graph activities controller

--- synprov/graph/controllers/activities_controller.py
+++ synprov/graph/controllers/activities_controller.py
@@ -3,12 +3,21 @@ import six
 import uuid
 import humps
 import json
+import logging
 
 from py2neo import Node, NodeMatcher
+from luqum.parser import parser
 
+from synprov.models.activity import Activity
 from synprov.config import neo4j_connection as graph
 from synprov.graph import ActivityBuilder, ActivityEditor
-from synprov.util import neo4j_to_d3, neo4j_export, convert_keys
+from synprov.util import neo4j_to_d3, neo4j_export, convert_keys, quote_string
+
+
+logger = logging.getLogger(__name__)
+
+
+ATTR_MAP = dict([[v, k] for k, v in Activity().attribute_map.items()])
 
 
 def add_activity_used(
@@ -101,7 +110,12 @@ def delete_activity_used(
     )
 
 
-def get_activities_graph(sort_by=None, order=None, limit=None):  # noqa: E501
+def get_activities_graph(
+    sort_by='created_at',
+    order='desc',
+    limit=3,
+    q='*:*'
+):  # noqa: E501
     """Get provenance graph
 
     Retrieve all nodes and relationships in the graph that pass filters.  # noqa: E501
@@ -112,12 +126,22 @@ def get_activities_graph(sort_by=None, order=None, limit=None):  # noqa: E501
     :type order: str
     :param limit: maximum number of connected activities to return
     :type limit: int
+    :param q: filter results using Lucene Query Syntax in the format of propertyName:value, propertyName:[num1 TO num2] and date range format, propertyName:[yyyyMMdd TO yyyyMMdd]
+    :type q: str
 
     :rtype: Neo4jGraph
     """
+    filter_properties = ''
+    if q != '*:*':
+        tree = parser.parse(q)
+        key = ATTR_MAP[tree.name]
+        val = quote_string(tree.children[0].value)
+        filter_properties = f' {{{key}: {val}}}'
+    logger.info(f"FILTER PROPS: {filter_properties}")
+
     query_base = (
         '''
-        MATCH (s:Activity)
+        MATCH (s:Activity{filter})
         WITH s
         ORDER BY s.{key}{dir}
         WITH collect(s) as activities
@@ -128,7 +152,8 @@ def get_activities_graph(sort_by=None, order=None, limit=None):  # noqa: E501
     ).format(
         key=sort_by,
         dir=(' ' + order.upper()) if order == 'desc' else '',
-        lim=limit
+        lim=limit,
+        filter=filter_properties
     )
 
     results = graph.run(
@@ -141,7 +166,8 @@ def get_agent_subgraph(
     user_id,
     sort_by='created_at',
     order='desc',
-    limit=3
+    limit=3,
+    q='*:*'
 ):  # noqa: E501
     """Get subgraph connected to an agent
 
@@ -155,12 +181,22 @@ def get_agent_subgraph(
     :type order: str
     :param limit: maximum number of connected activities to return
     :type limit: int
+    :param q: filter results using Lucene Query Syntax in the format of propertyName:value, propertyName:[num1 TO num2] and date range format, propertyName:[yyyyMMdd TO yyyyMMdd]
+    :type q: str
 
     :rtype: Neo4jGraph
     """
+    filter_properties = ''
+    if q != '*:*':
+        tree = parser.parse(q)
+        key = ATTR_MAP[tree.name]
+        val = quote_string(tree.children[0].value)
+        filter_properties = f' {{{key}: {val}}}'
+    logger.info(f"FILTER PROPS: {filter_properties}")
+
     query_base = (
         '''
-        MATCH (t:Agent {{user_id: {{user_id}}}})<-[r:WASASSOCIATEDWITH]-(s:Activity)
+        MATCH (t:Agent {{user_id: {{user_id}}}})<-[r:WASASSOCIATEDWITH]-(s:Activity{filter})
         WITH s
         ORDER BY s.{key}{dir}
         WITH collect(s) as activities
@@ -171,7 +207,8 @@ def get_agent_subgraph(
     ).format(
         key=sort_by,
         dir=(' ' + order.upper()) if order == 'desc' else '',
-        lim=limit
+        lim=limit,
+        filter=filter_properties
     )
 
     results = graph.run(
@@ -186,7 +223,8 @@ def get_reference_subgraph(
     direction='down',
     sort_by='created_at',
     order='desc',
-    limit=3
+    limit=3,
+    q='*:*'
 ):  # noqa: E501
     """Get subgraph connected to an entity
 
@@ -202,16 +240,26 @@ def get_reference_subgraph(
     :type order: str
     :param limit: maximum number of connected activities to return
     :type limit: int
+    :param q: filter results using Lucene Query Syntax in the format of propertyName:value, propertyName:[num1 TO num2] and date range format, propertyName:[yyyyMMdd TO yyyyMMdd]
+    :type q: str
 
     :rtype: Neo4jGraph
     """
+    filter_properties = ''
+    if q != '*:*':
+        tree = parser.parse(q)
+        key = ATTR_MAP[tree.name]
+        val = quote_string(tree.children[0].value)
+        filter_properties = f' {{{key}: {val}}}'
+    logger.info(f"FILTER PROPS: {filter_properties}")
+
     direction_rels = {
         'up': '-[r:WASGENERATEDBY]->',
         'down': '<-[r:USED]-'
     }
     query_base = (
         '''
-        MATCH (t:Reference {{target_id: {{target_id}}}}){dir_rel}(s:Activity)
+        MATCH (t:Reference {{target_id: {{target_id}}}}){dir_rel}(s:Activity{filter})
         WITH s
         ORDER BY s.{key}{dir}
         WITH collect(s) as activities
@@ -223,7 +271,8 @@ def get_reference_subgraph(
         dir_rel=direction_rels[direction],
         key=sort_by,
         dir=(' ' + order.upper()) if order == 'desc' else '',
-        lim=limit
+        lim=limit,
+        filter=filter_properties
     )
 
     results = graph.run(
